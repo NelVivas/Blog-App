b211ece40499531c473ab50c11df5a94e43082e5
Nelson Vivas <nelsonandresvivaslobo@gmail.com>
2019-06-20 11:07:08 -0400

    initial commit

---
+++ blog/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Post(models.Model):
+    """Model definition for Post."""
+
+    # TODO: Define fields here
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+    body = models.TextField()
+
+    class Meta:
+        """Meta definition for Post."""
+
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    def __str__(self):
+        """Unicode representation of Post."""
+        return self.title
